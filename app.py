3c404ad99baa208f4751f20378c70d0118a48590
Jidenius <jidenius@live.se>
2026-01-29 10:19:29 +0100

    Färdiga kommentarer osv

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hemlis!'
 socketio = SocketIO(app)
 
-# Vårt "minne" - en lista som sparar meddelande-objekt
 chat_history = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Autentisering
     if request.method == 'POST':
         password_attempt = request.form.get('password')
         username = request.form.get('username')
@@ -31,17 +31,16 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
-    # När en ny användare ansluter, skicka hela historiken bara till dem
+    # När en ny användare ansluter, skicka hela historiken
     print("En ny användare anslöt. Skickar historik...")
     emit('history', chat_history)
 
 @socketio.on('message')
 def handle_message(data):
+    # Spara meddelandet i vår lista och skicka det till alla anslutna användare
     data['time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # Spara meddelandet i vår lista
     chat_history.append(data)
     
-    # Håll historiken lagom stor (t.ex. de senaste 50 meddelandena)
     if len(chat_history) > 50:
         chat_history.pop(0)
         
@@ -50,10 +49,11 @@ def handle_message(data):
 
 @socketio.on('clear_chat')
 def handle_clear():
+    # Rensa chatthistoriken
     global chat_history
-    chat_history = []  # Tömmer listan på servern
+    chat_history = []  
     print("Chatten har rensats av en användare.")
-    # Skicka en signal till ALLA klienter att de ska rensa sina skärmar
+    
     emit('chat_cleared', broadcast=True)
 
 if __name__ == '__main__':
